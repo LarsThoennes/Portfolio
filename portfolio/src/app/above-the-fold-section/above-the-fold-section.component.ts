2d9084cc19a376630460a4de7e86c1e61c20d16f
LarsThoennes <larsthoennes@gmail.com>
2024-03-02 14:46:47 +0100

    html and css design for the above-the-fold component

---
+++ portfolio/src/app/above-the-fold-section/above-the-fold-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-above-the-fold-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './above-the-fold-section.component.html',
+  styleUrl: './above-the-fold-section.component.scss'
+})
+export class AboveTheFoldSectionComponent {
+
+}
