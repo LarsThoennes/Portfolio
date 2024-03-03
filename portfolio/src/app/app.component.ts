3933c155f8fddfd9a2b0a28493e7a42ff2800779
LarsThoennes <larsthoennes@gmail.com>
2024-03-03 01:05:19 +0100

    about-me component created. keyframe animation created in the above-the-folder component

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboveTheFoldSectionComponent } from './above-the-fold-section/above-the-fold-section.component';
+import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboveTheFoldSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, AboveTheFoldSectionComponent, AboutMeSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
