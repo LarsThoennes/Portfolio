bb580d0db1ef02d5c7a0bd63bbdf0a417069a899
LarsThoennes <larsthoennes@gmail.com>
2024-03-04 13:40:50 +0100

    Revision of the my-skills-section component and the animation of the icons

--- portfolio/src/app/my-skills/my-skills.component.ts
+++ portfolio/src/app/my-skills/my-skills.component.ts
@@ -13,4 +13,16 @@ export class MySkillsComponent {
   'Property 1=JavScript.png','Property 1=html.png','Property 1=css.png',
   'Property 1=Firebase.png','Property 1=Git.png','Property 1=Wordpress.png',
   'Property 1=Scrum.png','Property 1=Api.png','Property 1=Test Automation.png'];
+
+  application_names = ['Angular','TypeScript','JavaScript','HTML','CSS','Firebase',
+  'Git','Wordpress','Scrum','Rest-Api','Material design'];
+
+  combinedData: { icon: string, name: string }[] = [];
+
+ngOnInit() {
+  for (let i = 0; i < this.application_icons.length; i++) {
+    this.combinedData.push({ icon: this.application_icons[i], name: this.application_names[i] });
+  }
 }
+}
+
