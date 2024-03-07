83ed92cbbe1bb9df80a3e594b0cfaa5e8db53309
LarsThoennes <larsthoennes@gmail.com>
2024-03-07 17:58:33 +0100

    Added footer-section component and further edited the validation of the email form

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,14 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import * as AOS from 'aos';
 
 export const initAOS = () => {
   AOS.init();
 };
 
-
-import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),provideHttpClient()]
 };
